eb87fe69c71df6a319fa47de884d1cc5a9a0403a
okam1 <chornenkyy@gmail.com>
2015-07-29 14:51:37 +0300

    added default Role repository

---
+++ JobsUkraine/src/main/java/ua/com/jobsukraine/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package ua.com.jobsukraine.repository;
+
+import javax.management.relation.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+
+}
