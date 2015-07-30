5e81e3e99cc779ec19c9f79d32039316ec76027c
kardash <kardashr@i.ua>
2015-07-30 16:46:23 +0300

    add new register form for employers

--- JobsUkraine/src/main/java/ua/com/jobsukraine/controllers/EmployerController.java
+++ JobsUkraine/src/main/java/ua/com/jobsukraine/controllers/EmployerController.java
@@ -1,46 +1,72 @@
 package ua.com.jobsukraine.controllers;
 
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import ua.com.jobsukraine.entity.Category;
 import ua.com.jobsukraine.entity.Employer;
+import ua.com.jobsukraine.service.CategoryService;
 import ua.com.jobsukraine.service.EmployerService;
 import ua.com.jobsukraine.service.LoginInfoService;
 import ua.com.jobsukraine.service.RoleService;
 
 @Controller
 @ComponentScan("ua.com.jobsukraine.service")
+@SessionAttributes(types=Employer.class)
 public class EmployerController {
 
 	@Autowired
-	private EmployerService es;
+	private EmployerService employerService;
 	@Autowired
-	private LoginInfoService lis;
+	private LoginInfoService LoginInfoService;
 	@Autowired
-	private RoleService rs;
+	private RoleService roleService;
+	
+	@Autowired
+	private CategoryService categoryService;
 	
 	@RequestMapping(value = "/regEmployer", method = RequestMethod.GET)
-	public String addEmployer(Map<String, Object> model) {
+	public String addEmployerLogin(Model model) {
 
-		model.put("empForm", new Employer());
+		model.addAttribute("empForm", new Employer());
 
-		return "regEmployer";
+		return "regemp/RegEmpOne";
 	}
-
-	@RequestMapping(value = "/regEmployerNew", method = RequestMethod.POST)
-	public String register(@ModelAttribute("empForm")Employer empForm, BindingResult result) {
-
+	
+	@RequestMapping(value="/addEmployerInfo", method = RequestMethod.POST )
+    public String addEmployerInfo(Employer emp){
+		System.out.println(emp);
+		return "regemp/RegEmpTwo";
 		
-		empForm.getInfo().setRole(rs.findByName("employer"));
-		lis.add(empForm.getInfo());
-		es.add(empForm);
+	}
+	
+	@RequestMapping(value="/addEmpCategory", method = RequestMethod.POST)
+    public String addEmpCategory(Employer emp, Model model){
+		List<Category> listCat = categoryService.getAll();
+		
+		model.addAttribute("category", listCat);
+		return "regemp/regEmpAddCategory";
+		
+	}
+	
+	
+	@RequestMapping(value = "/regEmployerNew", method = RequestMethod.POST)
+	public String register(/*@ModelAttribute("empForm")*/Employer empForm, BindingResult result) {
+
+		System.out.println(empForm);
+		System.out.println(empForm.getInfo().toString());
+		empForm.getInfo().setRole(roleService.findByName("employer"));
+		LoginInfoService.add(empForm.getInfo());
+		employerService.add(empForm);
 		return "welcome";
 
 	}
