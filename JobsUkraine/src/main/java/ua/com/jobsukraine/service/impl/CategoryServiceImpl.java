76f49f93afc7d092cec951e39eaf89feab9dbee8
User <User@PC>
2015-07-30 13:46:14 +0300

    add Category service

---
+++ JobsUkraine/src/main/java/ua/com/jobsukraine/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,53 @@
+package ua.com.jobsukraine.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ua.com.jobsukraine.entity.Category;
+import ua.com.jobsukraine.repository.CategoryRepository;
+import ua.com.jobsukraine.service.CategoryService;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService{
+
+
+	@Autowired
+	private CategoryRepository cr;
+	
+	@Override
+	public Category add(Category obj) {
+		return cr.save(obj);
+	}
+
+	@Override
+	public void delete(int id) {
+
+		cr.delete(id);
+	}
+
+	@Override
+	public Category edit(Category obj) {
+		return cr.saveAndFlush(findByName(obj.getName()));
+
+	}
+
+	@Override
+	public Category find(int id) {
+		return cr.findOne(id);
+	}
+
+	@Override
+	public List<Category> getAll() {
+		return cr.findAll();
+	}
+
+	@Override
+	public Category findByName(String name) {
+		return cr.findByName(name);
+	}
+
+}
