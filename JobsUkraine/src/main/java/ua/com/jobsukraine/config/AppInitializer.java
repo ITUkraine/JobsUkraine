65c900de7b3725f965faaebab685a32fcc6a8e74
okam1 <chornenkyy@gmail.com>
2015-08-03 18:33:13 +0300

    added support of error-page for http 4xx errors

--- JobsUkraine/src/main/java/ua/com/jobsukraine/config/AppInitializer.java
+++ JobsUkraine/src/main/java/ua/com/jobsukraine/config/AppInitializer.java
@@ -1,5 +1,11 @@
 package ua.com.jobsukraine.config;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.util.Assert;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -18,4 +24,31 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 		return new String[] { "/" };
 	}
 
+	@Override
+    protected void registerDispatcherServlet(ServletContext servletContext) {
+
+        String servletName = super.getServletName();
+        Assert.hasLength(servletName, "getServletName() may not return empty or null");
+
+        WebApplicationContext servletAppContext = super.createServletApplicationContext();
+        Assert.notNull(servletAppContext,
+                "createServletApplicationContext() did not return an application " +
+                "context for servlet [" + servletName + "]");
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
+
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+
+        ServletRegistration.Dynamic registration = servletContext.addServlet(servletName, dispatcherServlet);
+        Assert.notNull(registration,
+                "Failed to register servlet with name '" + servletName + "'." +
+                "Check if there is another servlet registered under the same name.");
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping(getServletMappings());
+        registration.setAsyncSupported(isAsyncSupported());
+
+        super.customizeRegistration(registration);
+    }
+	
 }
