ff71a77b971583a6bfa25ec98ef6bdcae198dbed
Volodia Chornenkiy <chornenkyy@gmail.com>
2015-08-05 23:57:45 +0300

    simplify exception throwing for 404 page

--- JobsUkraine/src/main/java/ua/com/jobsukraine/config/AppInitializer.java
+++ JobsUkraine/src/main/java/ua/com/jobsukraine/config/AppInitializer.java
@@ -1,11 +1,7 @@
 package ua.com.jobsukraine.config;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
-import org.springframework.util.Assert;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import ua.com.jobsukraine.security.WebSecurityConfig;
@@ -27,30 +23,8 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	}
 
 	@Override
-    protected void registerDispatcherServlet(ServletContext servletContext) {
+	public void customizeRegistration(ServletRegistration.Dynamic registration) {
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+	}
 
-        String servletName = super.getServletName();
-        Assert.hasLength(servletName, "getServletName() may not return empty or null");
-
-        WebApplicationContext servletAppContext = super.createServletApplicationContext();
-        Assert.notNull(servletAppContext,
-                "createServletApplicationContext() did not return an application " +
-                "context for servlet [" + servletName + "]");
-
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
-
-        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
-
-        ServletRegistration.Dynamic registration = servletContext.addServlet(servletName, dispatcherServlet);
-        Assert.notNull(registration,
-                "Failed to register servlet with name '" + servletName + "'." +
-                "Check if there is another servlet registered under the same name.");
-
-        registration.setLoadOnStartup(1);
-        registration.addMapping(getServletMappings());
-        registration.setAsyncSupported(isAsyncSupported());
-
-        super.customizeRegistration(registration);
-    }
-	
 }
--- JobsUkraine/src/main/java/ua/com/jobsukraine/controllers/GlobalErrorController.java
+++ JobsUkraine/src/main/java/ua/com/jobsukraine/controllers/GlobalErrorController.java
@@ -1,15 +1,18 @@
 package ua.com.jobsukraine.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalErrorController {
 
 	public static final String DEFAULT_ERROR_VIEW = "errors/default";
 
-	@ExceptionHandler(Throwable.class)
+	@ExceptionHandler(Exception.class)
 	public ModelAndView exception(Exception e) {
 		e.printStackTrace();
 		ModelAndView mav = new ModelAndView(DEFAULT_ERROR_VIEW);
@@ -19,4 +22,13 @@ public class GlobalErrorController {
 		return mav;
 	}
 
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ModelAndView handleError404(HttpServletRequest request, Exception e) {
+		e.printStackTrace();
+		ModelAndView mav = new ModelAndView(DEFAULT_ERROR_VIEW);
+		mav.addObject("name", "404");
+		mav.addObject("message", "Page " + request.getRequestURL() + " not found");
+
+		return mav;
+	}
 }
