25884f58490d36ce1656eaa0a719b8e4315eeb97
okam1 <chornenkyy@gmail.com>
2015-08-10 12:31:50 +0300

    rewrite validation test due to java convention + delete empty package

--- JobsUkraine/src/main/test/ua/com/jobsukraine/entity/LoginInfoTest.java
+++ JobsUkraine/src/main/test/ua/com/jobsukraine/entity/LoginInfoTest.java
@@ -10,6 +10,7 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -27,84 +28,87 @@ public class LoginInfoTest {
 	public static void init() {
 		ValidatorFactory vf = Validation.buildDefaultValidatorFactory();
 		validator = vf.getValidator();
+	}
 
+	@Before
+	public void reinit() {
 		loginInfo = new LoginInfo();
 	}
 
 	@Test
 	public void isPasswordValid() {
-		String fieldName_password = "password";
+		String fieldNamePassword = "password";
 
 		// valid confirmPassword
 		loginInfo.setPassword("qwerty123");
-		assertTrue(validator.validateProperty(loginInfo, fieldName_password).isEmpty());
+		assertTrue(validator.validateProperty(loginInfo, fieldNamePassword).isEmpty());
 
 		// null
 		loginInfo.setPassword(null);
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_password);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNamePassword);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(NOT_NULL_MSG.equals(constraintViolations.iterator().next().getMessage()));
-		
-		//wrong format
+
+		// wrong format
 		loginInfo.setPassword("qwer"); // from 6
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_password);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNamePassword);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(WRONG_PASSWORD_LENGTH_MSG.equals(constraintViolations.iterator().next().getMessage()));
 	}
 
 	@Test
 	public void isConfirmPasswordValid() {
-		String fieldName_confirmPassword = "confirmPassword";
+		String fieldNameConfirmPassword = "confirmPassword";
 
 		// valid confirmPassword
 		loginInfo.setConfirmPassword("qwerty123");
-		assertTrue(validator.validateProperty(loginInfo, fieldName_confirmPassword).isEmpty());
+		assertTrue(validator.validateProperty(loginInfo, fieldNameConfirmPassword).isEmpty());
 
 		// null
 		loginInfo.setConfirmPassword(null);
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_confirmPassword);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNameConfirmPassword);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(NOT_NULL_MSG.equals(constraintViolations.iterator().next().getMessage()));
-		
-		//wrong format
+
+		// wrong format
 		loginInfo.setConfirmPassword("qwer"); // from 6
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_confirmPassword);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNameConfirmPassword);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(WRONG_PASSWORD_LENGTH_MSG.equals(constraintViolations.iterator().next().getMessage()));
 	}
 
 	@Test
 	public void isLoginValid() {
-		String fieldName_login = "login";
+		String fieldNameLogin = "login";
 
 		// valid login
 		loginInfo.setLogin("temnoi123");
-		assertTrue(validator.validateProperty(loginInfo, fieldName_login).isEmpty());
+		assertTrue(validator.validateProperty(loginInfo, fieldNameLogin).isEmpty());
 
 		// null
 		loginInfo.setLogin(null);
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_login);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNameLogin);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(NOT_NULL_MSG.equals(constraintViolations.iterator().next().getMessage()));
 
 		// wrong login format
 		loginInfo.setLogin("кирилицюНеМожна"); // [a-zA-Z0-9]+
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_login);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNameLogin);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(WRONG_LOGIN_MSG.equals(constraintViolations.iterator().next().getMessage()));
-		
+
 		loginInfo.setLogin("Cant_use_underscores"); // [a-zA-Z0-9]+
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_login);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNameLogin);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(WRONG_LOGIN_MSG.equals(constraintViolations.iterator().next().getMessage()));
-		
+
 		loginInfo.setLogin("tes"); // from 4 to 20
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_login);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNameLogin);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(WRONG_LOGIN_LENGTH_MSG.equals(constraintViolations.iterator().next().getMessage()));
-		
+
 		loginInfo.setLogin("123456789012345678901"); // from 4 to 20
-		constraintViolations = validator.validateProperty(loginInfo, fieldName_login);
+		constraintViolations = validator.validateProperty(loginInfo, fieldNameLogin);
 		assertFalse(constraintViolations.isEmpty());
 		assertTrue(WRONG_LOGIN_LENGTH_MSG.equals(constraintViolations.iterator().next().getMessage()));
 	}
