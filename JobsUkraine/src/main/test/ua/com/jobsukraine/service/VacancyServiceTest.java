0ea21def94a6c57b167c48504058795e23b2ac08
Dytyniak <vadym.dytyniak@gmail.com>
2015-08-13 16:29:15 +0300

    Added VacancyServiceTest without test for delete method

--- JobsUkraine/src/main/test/ua/com/jobsukraine/service/VacancyServiceTest.java
+++ JobsUkraine/src/main/test/ua/com/jobsukraine/service/VacancyServiceTest.java
@@ -1,21 +1,20 @@
 package ua.com.jobsukraine.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import javax.transaction.Transactional;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import ua.com.jobsukraine.config.HSQLTestConfig;
 import ua.com.jobsukraine.config.TestConfig;
 import ua.com.jobsukraine.entity.Category;
-import ua.com.jobsukraine.entity.Employer;
 import ua.com.jobsukraine.entity.Vacancy;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,55 +25,34 @@ public class VacancyServiceTest {
 	@Autowired
 	private VacancyService vacancyService;
 	@Autowired
-	private CategoryService categoryService;
-	@Autowired
-	private EmployerService employerService;
+	private JdbcTemplate jdbcTemplate;
 
-	private Vacancy vacancy;
-	
-	private Category category;
-	
-	private Employer employer;
-	
-	private Vacancy vacancyInDB;
-	
-	@Before
-	public void init(){
-		vacancy = new Vacancy();
-		category = new Category("Java");
-		categoryService.save(category);
-		employer = new Employer();
-		employer.setName("JobsUkraine");
-		employer.setAddress("Lviv");
-		employer.setDescription("Gooood");
-		employer.setEmail("employer@gmail.com");
-		employer.setPhone("0639631909");
-		employerService.save(employer);
-		vacancy.setName("Senior");
-		vacancy.setDescription("Good job");
-		vacancy.setSalary(1000);
-		vacancy.setCategory(category);
-		
-	}
-	
 	@Test
 	public void isVacancySaved() {
-		vacancyInDB = vacancyService.save(employer, vacancy);
-		assertEquals(vacancy, vacancyInDB);
+		try {
+			jdbcTemplate.execute("INSERT INTO category VALUES (1, 'Java')");
+			assertTrue(jdbcTemplate.queryForObject("SELECT COUNT(*) FROM vacancy", Integer.class) == 0);
+			Vacancy vacancy = new Vacancy();
+			vacancy.setName("Senior");
+			vacancy.setDescription("Bid salary");
+			vacancy.setCategory(new Category("Java"));
+			vacancyService.save(null, vacancy);
+			assertTrue(jdbcTemplate.queryForObject("SELECT COUNT(*) FROM vacancy", Integer.class) == 1);
+		} finally {
+			jdbcTemplate.execute("DELETE FROM vacancy");
+			jdbcTemplate.execute("DELETE FROM category");
+		}
 	}
-	
+
 	@Test
-	public void isVacancyFindById(){
-		vacancyInDB =  vacancyService.save(employer,vacancy);
-		assertEquals(vacancyService.find(vacancyInDB.getId()), vacancyInDB);
-	}
-	
-	@Test
-	public void isVacancyDeleted(){
-		vacancyInDB = vacancyService.save(employer,vacancy);
-		vacancyService.delete(vacancyInDB.getId());
-		assertNull(vacancyService.find(vacancyInDB.getId()));
+	public void isVacancyFindById() {
+		try {
+			jdbcTemplate.execute(
+					"INSERT INTO vacancy VALUES (1, 'Full-stack Junior Java developer', 'Junior Java Developer', 3000, NULL, NULL)");
+			assertEquals(new Integer(1), vacancyService.find(1).getId());
+		} finally {
+			jdbcTemplate.execute("DELETE FROM vacancy");
+		}
 	}
 
 }
- 
