cd54f481acb6219148fef2b80484d17cfd7124ea
Dytyniak <vadym.dytyniak@gmail.com>
2015-08-15 14:24:35 +0300

    Added upload collections of employers and candidate in jsp

--- JobsUkraine/src/main/java/ua/com/jobsukraine/controllers/AdminController.java
+++ JobsUkraine/src/main/java/ua/com/jobsukraine/controllers/AdminController.java
@@ -1,14 +1,34 @@
 package ua.com.jobsukraine.controllers;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ua.com.jobsukraine.entity.Candidate;
+import ua.com.jobsukraine.service.CandidateService;
+import ua.com.jobsukraine.service.EmployerService;
 
 @Controller
 @ComponentScan("ua.com.jobsukraine.service")
 public class AdminController {
+	
+	@Autowired
+	private EmployerService employerService;
+	@Autowired
+	private CandidateService candidateService;
+	
 	@RequestMapping(value="/admin")
-	public String goAdminPage(){
-		return "admin";
+	public ModelAndView goAdminPage(){
+		ModelAndView modelAndView = new ModelAndView("admin");
+		List<Candidate> candidates = candidateService.getAll();
+		Collections.sort(candidates);
+		modelAndView.addObject("employers", employerService.getAll());
+		modelAndView.addObject("candidates", candidates);
+		return modelAndView;
 	}
 }
