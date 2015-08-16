7eb2623c39a94728e2609d6e270e342259222550
okam1 <chornenkyy@gmail.com>
2015-08-16 11:05:11 +0300

    update AdminControllerTest

--- JobsUkraine/src/main/test/ua/com/jobsukraine/controller/AdminControllerTest.java
+++ JobsUkraine/src/main/test/ua/com/jobsukraine/controller/AdminControllerTest.java
@@ -1,7 +1,11 @@
 package ua.com.jobsukraine.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +17,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.View;
 
 import ua.com.jobsukraine.controllers.AdminController;
+import ua.com.jobsukraine.service.CandidateService;
+import ua.com.jobsukraine.service.EmployerService;
 
 public class AdminControllerTest {
 
@@ -21,6 +27,10 @@ public class AdminControllerTest {
 
 	@Mock
 	private View mockView;
+	@Mock
+	private EmployerService employerService;
+	@Mock
+	private CandidateService candidateService;
 
 	private MockMvc mockMvc;
 
@@ -32,7 +42,10 @@ public class AdminControllerTest {
 
 	@Test
 	public void testGoToAdminPage() throws Exception {
-		mockMvc.perform(get("/admin")).andExpect(status().isOk()).andExpect(view().name("admin"));
+		mockMvc.perform(get("/admin")).andExpect(status().isOk()).andExpect(view().name("admin"))
+				.andExpect(model().attributeExists("employers", "candidates")).andExpect(model().size(2));
+		verify(employerService, times(1)).getAll();
+		verify(candidateService, times(1)).getAll();
 	}
 
 }
