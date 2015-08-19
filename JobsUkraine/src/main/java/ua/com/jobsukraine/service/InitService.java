afbd968b5fc70c7d79f783bc8a3f86d2bf648e41
okam1 <chornenkyy@gmail.com>
2015-08-19 13:30:07 +0300

    update InitService

--- JobsUkraine/src/main/java/ua/com/jobsukraine/service/InitService.java
+++ JobsUkraine/src/main/java/ua/com/jobsukraine/service/InitService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ua.com.jobsukraine.entity.Category;
 import ua.com.jobsukraine.entity.Role;
 
 @Service
@@ -15,20 +14,10 @@ public class InitService {
 	@Autowired
 	private RoleService roleService;
 
-	@Autowired
-	private CategoryService catService;
-
 	@PostConstruct
 	public void init() {
-
 		roleService.save(new Role("ROLE_ADMIN"));
 		roleService.save(new Role("ROLE_EMPLOYER"));
 		roleService.save(new Role("ROLE_CANDIDATE"));
-
-		catService.save(new Category("Java"));
-		catService.save(new Category("HTML"));
-		catService.save(new Category("CSS"));
-		catService.save(new Category("Pyton"));
-		catService.save(new Category("Spring"));
 	}
 }
